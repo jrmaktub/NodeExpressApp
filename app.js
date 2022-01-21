1f14eec2d0facd89b251cccedf868c8878879401
jramirez09 <jramirez12@luc.edu>
2022-01-21 09:09:22 -0600

    serve a JSON object and display it

--- app.js
+++ app.js
@@ -2,11 +2,29 @@ let express = require('express')
 
 let app = express();
 
+// let person = {
+//     'Name' : 'Bob',
+//     'Age' : 20
+// }
+
+
+app.get('/person', (request, response)=>{
+    response.json(person)
+})
+
 app.listen(3000)
+
+
+
+app.get('/hello', (request, response)=> {
+    response.sendFile(__dirname + '/index.html')
+})
 
 // when there is a request to /pics, it will replace that slash with the other path
 app.use('/pics', express.static(__dirname + '/images'))
 
-app.get('/hello', (request, response)=> {
-    response.sendFile(__dirname + '/index.html')
+let message = {"message": 'Hello json'}
+
+app.get('/json', (request, response)=>{
+    response.json(message)
 })
