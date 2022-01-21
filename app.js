2d3746a6b40c1754d066b69afd9d4a479d61ddb0
jramirez09 <jramirez12@luc.edu>
2022-01-21 08:54:06 -0600

    installed middleware and tested with images

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ let app = express();
 
 app.listen(3000)
 
-console.log(__dirname)
+// when there is a request to /pics, it will replace that slash with the other path
+app.use('/pics', express.static(__dirname + '/images'))
 
 app.get('/hello', (request, response)=> {
-    
     response.sendFile(__dirname + '/index.html')
 })
